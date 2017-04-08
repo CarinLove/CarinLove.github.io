4cabf9414cea55442b5a6021d3a6cc1cd830ce84
CarinLove <CarinLove@users.noreply.github.com>
2017-04-08 18:50:12 -0500

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -6,3 +6,27 @@ var today = new Date();
 //Display the date
 
 document.getElementById("Today").innerHTML = today.toDateString;
+
+//Data Validation
+function validateform() {
+    var firstName;
+    var lastName;
+    var eMail;
+
+    var firstName = document.forms["myForm"]["firstName"].value;
+    var lastName = document.forms["myForm"]["lastName"].value;
+    var eMail = document.forms["myForm"]["eMail"].value;
+
+    if (firstName=="") {
+        alert("All fields with '*' are required");
+        return false;
+    }
+    if (lastName== "") {
+        alert("All fields with '*' are required");
+        return false;
+    }
+    if (eMail== "") {
+        alert("All fields with '*' are required");
+        return false;
+    }
+}
